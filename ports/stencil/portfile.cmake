1f763184fa9eb1b103bec7392da280f75dc6a0d7
Ankur <ankurv@verma>
2024-11-24 09:04:57 -0800

    Squashed

--- ports/stencil/portfile.cmake
+++ ports/stencil/portfile.cmake
@@ -1,12 +1,14 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO ankurvdev/embedresource
-    REF "67b3313c073d55f084369df92d202b23185a65cc"
-    SHA512 697f3aa9a7d47f03b18ebd7e305f19b05203fdc0a62e77fac52c8863306c7386e48dc86e3caca8ced8b0bf913fe039046ab9bebac596105d9773f468b50cab72
+    REPO ankurvdev/stencil
+    REF "a928152888fb2832286afb6db9df912e5f192131"
+    SHA512 26b434bedac13a7774d27bcd091e732ee6a3e7b98a7beac9927dd5e5f6026ca98edb115ec384e3b3db552b9cdc333f5b939ea9f743733ff407bba603b860ea35
     HEAD_REF main)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
@@ -16,15 +18,14 @@ vcpkg_copy_pdbs()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 if(HOST_TRIPLET STREQUAL TARGET_TRIPLET) # Otherwise fails on wasm32-emscripten
-    vcpkg_copy_tools(TOOL_NAMES embedresource AUTO_CLEAN)
+    vcpkg_copy_tools(TOOL_NAMES stencil AUTO_CLEAN)
 else()
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
-file(READ "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake" config_contents)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake"
-"find_program(
-    embedresource_EXECUTABLE embedresource
+file(READ "${CURRENT_PACKAGES_DIR}/share/stencil/stencilConfig.cmake" config_contents)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/stencil/stencilConfig.cmake" "find_program(
+    stencil_EXECUTABLE stencil
     PATHS
         \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/${PORT}\"
     NO_DEFAULT_PATH
