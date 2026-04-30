9c7b4fb0ba8d4f029d004a87230643dfe88a867e
Ankur <ankurv@verma>
2026-04-30 21:05:52 +0000

    Squashed

---
+++ ports/stencil/portfile.cmake
@@ -0,0 +1,34 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ankurvdev/stencil
+    REF "f0e22da44c1aa01aabb66d0d849cae5d8d5c01ab"
+    SHA512 20928a23028ca98f0ac2584cecd9c66bdc9a5f9ea326461a50d6ac68674dafa80197c3b6e79ac8e334c848aa20447efa6f5bc7e04b35621e4b47af2a1c139186
+    HEAD_REF main)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+# Handle copyright
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+if(HOST_TRIPLET STREQUAL TARGET_TRIPLET) # Otherwise fails on wasm32-emscripten
+    vcpkg_copy_tools(TOOL_NAMES stencil AUTO_CLEAN)
+else()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+endif()
+
+file(READ "${CURRENT_PACKAGES_DIR}/share/stencil/stencilConfig.cmake" config_contents)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/stencil/stencilConfig.cmake" "find_program(
+    stencil_EXECUTABLE stencil
+    PATHS
+        \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/${PORT}\"
+    NO_DEFAULT_PATH
+    REQUIRED)
+${config_contents}"
+)
